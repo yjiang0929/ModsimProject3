adc006cbd5300698303703209dfd4028117bad0e
yjiang0929 <pauljyc0929@gmail.com>
2016-11-28 15:46:14 -0500

    flow functions

--- spiderman_drop_flow.m
+++ spiderman_drop_flow.m
@@ -6,9 +6,10 @@ function res = spiderman_drop_flow (t, W, spiderman_mass, g,...
     Vy = W(4);
     dxdt = Vx;
     dydt = Vy;
-    dVxdt = 0;
+    %Drag not considered now
+    dVxdt = 0 / spiderman_mass;
     %drag = 0.5 * drag_constant * air_density * contact_area *...
     %((x^2+y^2)^(0.5)) * y
-    dVydt = - spiderman_mass * g;
+    dVydt = - spiderman_mass * g / spiderman_mass;
     res = [dxdt;dydt;dVxdt;dVydt];
 end
--- spiderman_swing_flow.m
+++ spiderman_swing_flow.m
@@ -6,10 +6,12 @@ function res = spiderman_swing_flow (t, W, spiderman_mass, g, spring_constant,..
     Vy = W(4);
     dxdt = Vx;
     dydt = Vy;
-    dVxdt = - spring_constant * ((x^2+y^2)^(0.5)-initial_length) * x/((x^2+y^2)^(0.5));
+    %Drag not considered for now
+    dVxdt =(- spring_constant * ((x^2+y^2)^(0.5)-initial_length) * x/((x^2+y^2)^(0.5)))/...
+        spiderman_mass;
     %drag = 0.5 * drag_constant * air_density * contact_area *...
     %((x^2+y^2)^(0.5)) * y
-    dVydt = - spiderman_mass * g - spring_constant * ((x^2+y^2)^(0.5)-...
-        initial_length) * y/((x^2+y^2)^(0.5));
+    dVydt =(- spiderman_mass * g - spring_constant * ((x^2+y^2)^(0.5)-...
+        initial_length) * y/((x^2+y^2)^(0.5)))/spiderman_mass;
     res = [dxdt;dydt;dVxdt;dVydt];
 end
